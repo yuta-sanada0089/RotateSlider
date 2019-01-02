f783f498308a47cd10a31928481dad64dd6e63dd
真田雄太 <yuta_sanada@gdo.co.jp>
2019-01-03 07:14:12 +0900

    Initial Commit

---
+++ RotateSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RotateSlider
+//
+//  Created by mac on 2019/01/03.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
